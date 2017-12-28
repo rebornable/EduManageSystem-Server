f8b87f9555ad497314bf97ff9ecf036e81b4ac8e
panlulugit <17854268056@163.com>
2017-12-28 20:43:38 +0800

    addCourse

--- EMSystem/src/main/java/com/dao/ICourseDao.java
+++ EMSystem/src/main/java/com/dao/ICourseDao.java
@@ -7,4 +7,6 @@ import com.bean.Course;
 public interface ICourseDao {
    	List<Course> getCourse();
    	Course findCourse(String id);
+   	void addCourse(Course course);
+   	int deleteCourse(String id);
 }
